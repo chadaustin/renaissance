94b32869ec1e323dfdef8442dd519adb770285cf
Chad Austin <chad@chadaustin.me>
2011-12-24 01:28:49 -0600

    mac build support

--- ren2/build.sh
+++ ren2/build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-CC="g++ -std=c++0x -Wall -Werror -I."
+if [ -x /opt/local/bin/g++-mp-4.6 ]; then
+    GXX=/opt/local/bin/g++-mp-4.6
+else
+    GXX=g++
+fi
+CC="$GXX -std=c++0x -Wall -Werror -I."
 
 set -e
+mkdir -p bin
 $CC -o bin/Skinning.exe test/Skinning.cpp
 $CC -o bin/Passthrough.exe test/Passthrough.cpp
 $CC -o bin/Transform.exe test/Transform.cpp
